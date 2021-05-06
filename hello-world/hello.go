a2809a87ed0f51cc820bd0df31f92c52b51726ad
Tatsuro Miyamoto <mtatsu0130@gmail.com>
2021-05-06 19:53:17 +0900

    complete

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,14 +2,35 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	englishHelloPrefix = "Hello, "
+	spanish            = "Spanish"
+	spanishHelloPrefix = "Hola, "
+	french             = "French"
+	frenchHelloPrefix  = "Bonjour, "
+)
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
 }
+
+func greetingPrefix(language string) string {
+	var prefix string
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return prefix
+}
+
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
