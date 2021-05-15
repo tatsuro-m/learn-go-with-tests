3fb9607faac96190d43decc8ff86aebde621cf3c
Tatsuro Miyamoto <mtatsu0130@gmail.com>
2021-05-15 12:33:01 +0900

    complete

--- di/di.go
+++ di/di.go
@@ -3,21 +3,17 @@ package main
 import (
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 )
 
 func Greet(writer io.Writer, name string) {
-	_, err := fmt.Fprintf(writer, "Hello, %s", name)
-	if err != nil {
-		return
-	}
+	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
-func MyGreetHandler(w http.ResponseWriter, _ *http.Request) {
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
 	Greet(w, "world")
 }
 
 func main() {
-	log.Fatal(http.ListenAndServe(":5000", http.HandlerFunc(MyGreetHandler)))
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
 }
